c22d9b63d598ee68c49c718992fbdcfa94f239a6
Alejandro Villalobos <is728135@iteso.mx>
2021-02-17 19:31:56 -0600

    clase AFD con setters

--- src/AFD.java
+++ src/AFD.java
@@ -1,3 +1,21 @@
 public class AFD {
-    
+    public int actual;
+    public int inicial;
+    public int[] finales;
+    public int[] matriz;
+
+    public void setInicial(int inicial) {
+        this.inicial = inicial;
+        this.actual = inicial;
+    }
+
+    public void setFinales(int[] finales) {
+        this.finales = finales;
+    }
+
+    public void setMatriz(int[] matriz) {
+        this.matriz = matriz;
+    }
+
+
 }
